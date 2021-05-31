7585b6ce3e106842aa8839a3235833e64623356b
Moshiur Rahman <rmoshiur705@gmail.com>
2021-06-01 00:56:10 +0600

    Add and delete data with localstorage

--- Expense Tracker/app.js
+++ Expense Tracker/app.js
@@ -7,13 +7,41 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 
-let transactions = [
-    {id: 1, text: 'Cash', amount: 120 },
-    {id: 1, text: 'Salary', amount: -120 },
-    {id: 1, text: 'Deposit', amount: 23 },
-    {id: 1, text: 'Gift', amount: 21 },
-    {id: 1, text: 'Cash', amount: -15 },
-];
+const storageData = JSON.parse(localStorage.getItem('transactions'));
+
+let transactions = storageData !== null ? storageData : [];
+
+// Add Transactions
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() == '' || amount.value.trim() == ''){
+        alert('Please input all values');
+    }else {
+        const transaction = {
+          id: generateID(),
+          text: text.value,
+          amount: +amount.value
+        };
+
+        transactions.push(transaction);
+
+        addTransactionToDom(transaction);
+
+        updateValues();
+
+        updateLocalStorage();
+
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+// Generate random ID
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+  }
 
 // Add transactions to DOM
 function addTransactionToDom(transaction) {
@@ -23,7 +51,7 @@ function addTransactionToDom(transaction) {
 
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class='delete-btn'>x</button>
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class='delete-btn' onclick='removeTransaction(${transaction.id})'>x</button>
     `;
     list.appendChild(item); 
 }
@@ -44,6 +72,18 @@ function addTransactionToDom(transaction) {
         moneyPlus.innerText = `$${income}`;
         moneyMinus.innerText = `$${expense}`;
     }
+
+// Remove Transactions
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id != id);
+    updateLocalStorage();
+    init();
+}
+
+// update local storage
+    function updateLocalStorage() {
+        localStorage.setItem('transactions', JSON.stringify(transactions));
+    }
 // init
 
 function init() {
@@ -56,3 +96,5 @@ function init() {
 
 
 init();
+
+form.addEventListener('submit', addTransaction);
