7c1973d884fc85008d923f131ba4b66db04b3c18
A.H.M Mosiur Rahaman <a.h.m.mosiur.rahaman@portonics.com>
2024-08-24 07:20:43 +0600

    Sample accordion added

--- accordion/app.js
+++ accordion/app.js
@@ -1,27 +1,27 @@
-const data = [
-    {
-        title: 'hello',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'
-    },
-    {
-        title: 'hello2',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'
-    },
-    {
-        title: 'hello3',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'
-    }
-]
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const item = document.querySelectorAll('.item');
+    item.forEach((element, index) => {
+        console.log(element, index);
+
+        element.querySelector('.item-header').addEventListener('click', (event) => onClickQuestion(event, element))
+    })
+})
+
+function onClickQuestion(e, element) {
+    element.classList.toggle('active');
+}
 
 function prepareAccordion() {
     let html = ``
     data.map(d => {
-        html += `<div class="item active">
+        html += `<div class="item">
         <div class="item-header">
-            <h3 class="item-title">${d.title}</h3>
+            <h3 class="item-title">${d.qeustion}</h3>
             <span class="toggle-icon"><i class="fa-solid fa-truck-fast"></i></span>
         </div>
-            <div class="item-content">${d.desc}</div>
+            <div class="item-content">${d.answer}</div>
         </div>`
     })
 
---
+++ accordion/data.js
@@ -0,0 +1,209 @@
+var data = [
+    {
+        "id": 1,
+        "qeustion": "How can I contact you if I have any questions?",
+        "answer": "You can contact us at  or call us at 123-456-7890",
+        category: 1
+    },
+    {
+        "id": 2,
+        "qeustion": "Where can I purchase your products?",
+        "answer": "You can purchase our products online through our website or in store. Click  to see our retail locations and partners.",
+        category: 1
+    },
+    {
+        "id": 3,
+        "qeustion": "What should I do if I'm between sizes?",
+        "answer": "Athlete’s bodies come in all shapes and sizes. We’ve gone ahead and taken out the guesswork. If you are on the border between sizes, a smaller size will have stronger targeted compression than a larger size. You will receive the same support from the Embrace System™ regardless of which one you choose. We recommend you go with your usual size or your preferred compression.",
+        category: 1
+    },
+    {
+        "id": 4,
+        "qeustion": "How do I put on the K1?",
+        "answer": `<p>Click&nbsp;<a href="https://www.youtube.com/watch?v=T4ZzuvzPIXk">here</a>&nbsp;to watch our&nbsp;instructional&nbsp;video or read on below for written&nbsp;instructions.</p>
+        <p>&nbsp;</p>
+
+        <p><strong>Things to remember the first time you put your K1s on:</strong></p>
+
+        <p>&nbsp;</p>
+
+        <p><strong>The K1&nbsp;Is Tight!</strong></p>
+
+        <p dir="ltr">Sit down and inch them up like a wetsuit. Over time, they will conform to your body.&nbsp;</p>
+
+        <p dir="ltr">It could take 5 minutes or more to put the K1&nbsp;on for the first time.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p><strong>Tug on Those Tights!&nbsp;</strong></p>
+
+        <p dir="ltr">Pull firmly on the high-strength cables that run through the K1&nbsp;to work them up your legs.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p><strong>The K1&nbsp;Is Packed With Tech!</strong></p>
+
+        <p dir="ltr">There will be exposed cables at the ankles but they’re meant to be there.&nbsp;</p>
+
+        <p dir="ltr"><br>
+        &nbsp;</p>
+
+        <p dir="ltr"><strong>Putting on</strong></p>
+
+        <p dir="ltr"><strong>&nbsp;</strong></p>
+
+        <p dir="ltr">1. Pull the dials out to disengage the support. You should be able to twist them freely in both directions.&nbsp;&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">2. From inside the waistband, stretch outward to relax the Embrace System™. Pull firmly, you should feel the cables move.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">3. Sitting down, pull the K1&nbsp;over each foot. Position the cuffs at your ankles with the seam on the inside of your legs.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">4. Work the K1&nbsp;up to your knees. Position the criss-cross pattern at your mid-shins - it’s ok to pull the cables firmly.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">5. Continue working the K1&nbsp;up to your mid-thighs. Align the two light grey bars with the bend of your knee.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">6. Stand up and pull the waistband over your hips. This will be difficult if the light grey bars aren’t positioned correctly.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr"><strong>Adjusting</strong></p>
+
+        <p dir="ltr">&nbsp;</p>
+
+        <p dir="ltr">1. Push the dials in and rotate to engage support - you will feel hip pressure. The left dial controls the left leg, and right controls right.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">2. Stretch the waistband outward and move (squat, jog, etc.) to distribute support down your legs.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">3. The K1&nbsp;provides more support the tighter the cables are. Your support is personal - adjust according to comfort or medical recommendation.&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p dir="ltr">4. You will know your K1&nbsp;is&nbsp;working as you feel it tighten across your waist, thighs, and calves.&nbsp;</p>
+
+        <p dir="ltr"><strong>&nbsp;</strong></p>
+
+        <p dir="ltr"><strong>Removal</strong></p>
+
+        <p dir="ltr"><strong>&nbsp;</strong></p>
+
+        <p dir="ltr">1. Disengage the support by pulling the control dials out. Pull the waistband outwards to release the cables. Work the K1&nbsp;down your legs and over your feet.</p>`,
+        category: 2
+    },
+    {
+        "id": 5,
+        "qeustion": "How is the K1 different from traditional knee braces on the market?",
+        "answer": "",
+        category: 2
+    },
+    {
+        "id": 6,
+        "qeustion": "How do I know it's working?",
+        "answer": "You will know the K1 is working by the pressure you feel from the cables, and the support you feel when pushing into them at your knees. The K1 tightens from the waist down and, as a result, you will first feel the pressure at your hips - this is the first sign that it is working! As you begin to move, the support will distribute down your legs. To test it at your knees, plant your foot and gently push your knee inward - towards your midline. You should feel the cables around the knee tighten to resist the joint moving in this undesireable way. Continue to rotate the dials to your desired comfort and support level.",
+        category: 2
+    },
+    {
+        "id": 7,
+        "qeustion": "How do I know my K1 is on correctly?",
+        "answer": `The key three regions are hips, knees, and calves. First, ensure the waistband is pulled up over your hips and the cables in the calf swoop below the meat of your calf muscles. Now, focusing on the knee, there will be two light grey markers on either side of the knee. These markers should be centered just above where your knee bends, and point towards your kneecap. With the garment positioned correctly, it should also be supportive and comfortable! <p>The key three regions are hips, knees, and calves. First, ensure the waistband is pulled up over your hips and the cables in the calf swoop below the meat of your calf muscles. Now, focusing on the knee, there will be two light grey markers on either side of the knee. These markers should be centered&nbsp;just above where your knee bends, and point towards&nbsp;your kneecap. With the garment positioned correctly, it should also be supportive and comfortable!</p><p>Click&nbsp;<a href="https://www.youtube.com/watch?v=T4ZzuvzPIXk">here</a>&nbsp;to watch an instructional video.&nbsp;</p>Click here to watch an instructional video. `,
+        category: 2
+    },
+    {
+        "id": 8,
+        "qeustion": "What is the product intended for?",
+        "answer": ` <p>The K1&nbsp;is&nbsp;designed for all activity levels including those with medium to high impact and for cases including:</p>
+
+        <ul>
+            <li>
+            <p>Moderate to severe ligament instabilities (such as MCL, LCL, ACL, and PCL)</p>
+            </li>
+            <li>Combined mild strains and sprains</li>
+            <li>Instabilities of the knee</li>
+        </ul>`,
+        category: 2
+    },
+    {
+        "id": 9,
+        "qeustion": "Is the K1 a medical device?",
+        "answer": "Yes, the K1 is registered by the FDA and Health Canada as a Class 1 medical device.",
+        category: 2
+    },
+    {
+        "id": 10,
+        "qeustion": "What is your shipping policy?",
+        "answer": "We offer free standard shipping on all products. Please contact us at example@mail.com if you have any inquiries. Depending on your proximity to our distribution center in Vancouver, Canada, please expect anywhere from 5-10 business days for your order to arrive.",
+        category: 3
+    },
+    {
+        "id": 11,
+        "qeustion": "Which countries do you ship to?",
+        "answer": "At this time, Stoko ships product to Canada and the U.S. ",
+        category: 3
+    },
+    {
+        "id": 12,
+        "qeustion": "How can I track my order?",
+        "answer": "Following your purchase, you will receive an email confirmation. Stoko strives to process all orders, placed by 5:00pm PST, within 24hrs. Once your Stoko K1 is processed, you will receive an email with a tracking number for your order.",
+        category: 3
+    },
+    {
+        "id": 13,
+        "qeustion": "How do I change or cancel my order?",
+        "answer": "For any order cancellations or changes, please contact example@mail.com Stoko does not guarantee a change or cancelation can be made following order confirmation due to the stage of package processing and delivery.",
+        category: 3
+    },
+    {
+        "id": 14,
+        "qeustion": "What is the return policy?",
+        "answer": `You can return Stoko products within 30 days of purchasing. Please contact returns@stokodesign.com to process your return and obtain a return shipping label.`,
+        category: 3
+    },
+    {
+        "id": 15,
+        "qeustion": "What is the warranty policy?",
+        "answer": "Stoko guarantees that all products will be free of manufacturing defects for a period of 1 year following date of purchase. For any inquiries, please contact our team at warranty@stokodesign.com",
+        category: 4
+    },
+    {
+        "id": 16,
+        "qeustion": "Is Stoko covered by insurance?",
+        "answer": "Stoko has been recognized as an eligible medical product for reimbursement in Canada. Please refer to your extended benefits policy for further information as policy coverage varies.",
+        category: 5
+    }
+]
+
+var categories = [
+    {
+        "id": 1,
+        "title": "Sample"
+    },
+    {
+        "id": 2,
+        "title": "Product"
+    },
+    {
+        "id": 3,
+        "title": "Shipping"
+    },
+    {
+        "id": 4,
+        "title": "Warranty"
+    },
+    {
+        "id": 5,
+        "title": "Insurance"
+    }
+]
